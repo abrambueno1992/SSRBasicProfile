fdb4c9fd21de79343a894a5f27a4afcb9d1c1042
Abraham Bueno <abueno@localhost.localdomain>
2018-11-14 17:33:07 -0600

    Troubleshooting styles.

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,145 +1,225 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 import Link from 'next/link';
-import './Navigation.css'
-
+import './Navigation.css';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import withLayout from '../lib/withLayout';
 
 class Navigation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            index: null,
-            current: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: null,
+      current: '',
+    };
+  }
 
+  componentDidMount = () => {
+    let temp = this.props.current.split('/');
+    temp = temp[1].split('');
+    if (temp[0]) {
+      temp[0] = temp[0].toUpperCase();
+      let newtemp = '';
+      temp.forEach((element) => {
+        newtemp += element;
+      });
+      if (newtemp === 'Home') {
+        // this.setState({current: newtemp, index: 0})
+      } else if (newtemp === 'Projects') {
+        this.setState({ current: newtemp, index: 1 });
+      } else if (newtemp === 'About') {
+        this.setState({ current: newtemp, index: 2 });
+      } else if (newtemp === 'Blog') {
+        this.setState({ current: newtemp, index: 3 });
+      } else if (newtemp === 'Contact') {
+        this.setState({ current: newtemp, index: 4 });
+      } else {
+        this.setState({ current: newtemp, index: 0 });
+      }
+    } else {
+      this.setState({
+        index: 0,
+        current: 'Home',
+      });
     }
-    componentDidMount = () => {
-        let temp = this.props.current.split('/');
-        temp = temp[1].split('');
-        if (temp[0]) {
-            temp[0] = temp[0].toUpperCase();
-            let newtemp = '';
-            temp.forEach(element => {
-                newtemp += element;
-            });
-            if (newtemp === 'Home') {
-                // this.setState({current: newtemp, index: 0})
+  };
 
-            } else if (newtemp === 'Projects') {
-                this.setState({ current: newtemp, index: 1 })
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="Nav">
+        <ul className="breadcrumb">
+          {this.state.index !== 0 ? (
+            <div>
+              <li>
+                <Link href="/">
+                  <a>
+                    <Button
+                      style={{ marginRight: 10, marginLeft: 10 }}
+                      variant="contained"
+                      color="primary"
+                      size="small"
+                    >
+                      Home
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          ) : (
+            <div>
+              <li>
+                <Link href="/">
+                  <a>
+                    <Button
+                      style={{ marginRight: 10, marginLeft: 10 }}
+                      variant="contained"
+                      color="secondary"
+                      size="small"
+                    >
+                      Home
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          )}
 
-            } else if (newtemp === 'About') {
-                this.setState({ current: newtemp, index: 2 })
+          {this.state.index !== 1 ? (
+            <div>
+              <li>
+                <Link href="/projects">
+                  <a>
+                    <Button
+                      style={{ marginRight: 10, marginLeft: 10 }}
+                      variant="contained"
+                      color="primary"
+                      size="small"
+                    >
+                      Projects
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          ) : (
+            <div>
+              <li>
+                <Link href="/projects">
+                  <a>
+                    <Button
+                      style={{ marginRight: 10, marginLeft: 10 }}
+                      variant="contained"
+                      color="secondary"
+                      size="small"
+                    >
+                      Projects
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          )}
 
-            } else if (newtemp === 'Blog') {
-                this.setState({ current: newtemp, index: 3 })
-            } else if (newtemp === 'Contact') {
-                this.setState({ current: newtemp, index: 4 })
-            } else {
-                this.setState({ current: newtemp, index: 0 })
+          {this.state.index !== 2 ? (
+            <div>
+              <li>
+                <Link href="/about">
+                  <a>
+                    <Button
+                      style={{ marginRight: 10, marginLeft: 10 }}
+                      variant="contained"
+                      color="primary"
+                      size="small"
+                    >
+                      About
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          ) : (
+            <div>
+              <li>
+                <Link href="/about">
+                  <a>
+                    <Button
+                      style={{ marginRight: 10, marginLeft: 10 }}
+                      variant="contained"
+                      color="secondary"
+                      size="small"
+                    >
+                      About
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          )}
 
-            }
-        } else {
-            this.setState({
-                index: 0,
-                current: 'Home'
-            })
-        }
+          {this.state.index !== 3 ? (
+            <div>
+              <li>
+                <Link href="/blog">
+                  <a>
+                    <Button
+                      style={{ marginRight: 10, marginLeft: 10 }}
+                      variant="contained"
+                      color="primary"
+                      size="small"
+                    >
+                      Blog
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          ) : (
+            <div>
+              <li>
+                <Link href="/blog">
+                  <a>
+                    <Button
+                      style={{ marginRight: 10, marginLeft: 10 }}
+                      variant="contained"
+                      color="secondary"
+                      size="small"
+                    >
+                      Blog
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          )}
 
-    }
+          {this.state.index !== 4 ? (
+            <div>
+              <li>
+                <Link href="/contact">
+                  <a>
+                    <Button variant="contained" color="primary" size="small">
+                      Contact
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          ) : (
+            <div>
+              <li>
+                <Link href="/contact">
+                  <a>
+                    <Button variant="contained" color="secondary" size="small">
+                      Contact
+                    </Button>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          )}
 
-    render() {
-        return (
-            <div className="Nav">
-
-                <ul className="breadcrumb">
-                    {this.state.index !== 0 ?
-                        <div className="inactiveLink">
-                            <li>
-                                <Link href="/">
-                                    <a >Home</a>
-                                </Link>
-                            </li>
-                        </div>
-                        : 
-                        <div className="activeLink" >
-                        <li>
-                            <Link href="/">
-                                <a className="activeLinkInner" >Home</a>
-                            </Link>
-                        </li>
-                    </div>    
-                    }
-
-
-                    {this.state.index !== 1 ?
-                        <div className="inactiveLink">
-                            <li>
-                                <Link href="/projects">
-                                    <a >Projects</a>
-                                </Link>
-                            </li>
-                        </div> : 
-                        <div className="activeLink">
-                            <li>
-                                <Link href="/projects">
-                                    <a className="activeLinkInner" >Projects</a>
-                                </Link>
-                            </li>
-                        </div>}
-
-
-                    {this.state.index !== 2 ?
-                        <div className="inactiveLink">
-                            <li>
-                                <Link href="/about">
-                                    <a  >About</a>
-                                </Link>
-                            </li>
-                        </div> : 
-                        <div className="activeLink">
-                            <li>
-                                <Link href="/about">
-                                    <a className="activeLinkInner" >About</a>
-                                </Link>
-                            </li>
-                        </div>}
-
-
-                    {this.state.index !== 3 ?
-                        <div className="inactiveLink">
-                            <li>
-                                <Link href="/blog">
-                                    <a >Blog</a>
-                                </Link>
-                            </li>
-                        </div> : 
-                        <div className="activeLink">
-                            <li>
-                                <Link href="/blog">
-                                    <a className="activeLinkInner" >Blog</a>
-                                </Link>
-                            </li>
-                        </div>}
-
-
-                    {this.state.index !== 4 ?
-                        <div className="inactiveLink">
-                            <li>
-                                <Link href="/contact">
-                                    <a >Contact</a>
-                                </Link>
-                            </li>
-                        </div> : 
-                        <div className="activeLink">
-                            <li>
-                                <Link href="/contact">
-                                    <a className="activeLinkInner" >Contact</a>
-                                </Link>
-                            </li>
-                        </div>}
-
-                    {/* <div className="activeLink">
+          {/* <div className="activeLink">
                         <li >
                             <a className="activeLinkInner" >
                                 {this.state.current}
@@ -147,13 +227,11 @@ class Navigation extends Component {
                         </li>
 
                     </div> */}
-                </ul>
-                {/* </Breadcrumb> */}
-            </div>
-
-        )
-    }
+        </ul>
+        {/* </Breadcrumb> */}
+      </div>
+    );
+  }
 }
 
-
-export default Navigation
+export default Navigation;
--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,80 @@
-
 import Particles from 'react-particles-js';
-import Navigation from '../components/Navigation';
-import './Home.css'
-import Head from 'next/head'
-import React, { Component } from 'react'
-import fetch from 'isomorphic-unfetch'
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+import './Home.css';
+import Head from 'next/head';
+import React, { Component } from 'react';
+// import fetch from 'isomorphic-unfetch';
+// import Navigation from '../components/Navigation';
+import withLayout from '../lib/withLayout';
 
-  
-  render() {
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-    return (
-      <div className="Home" >
-        <Head>
-          <title>Portfolio Project</title>
-          <meta charSet='utf-8' />
-          <meta name="description" content="Portfolio " />
-          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-          {/* <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=3.0, minimum-scale=0.86"></meta> */}
-        </Head>
-        <div className="BreadCrumbIndex">
-          <Navigation current={this.props.url.pathname} />
-        </div>
-        <div className="Intro">
-          <div className="Text">Hello, my name is Abraham Bueno, and I'm a Full Stack Web Developer</div>
-          <div className="Text">The Stack I use is Mongo DB, Express, React, Redux, and Node.</div>
-        </div>
-        <div className="ParticlesStyleIndex">
-          <Particles
-            params={{
-              particles: {
-                number: {
-                  value: 70
-                },
-                line_linked: {
-                  shadow: {
-                    enable: true,
-                    color: "#3CA9D1",
-                    blur: 0
-                  }
-                }
-              },
-              interactivity: {
-                events: {
-                  onhover: {
-                    enable: true,
-                    mode: 'grab'
-                  }
-                }
+//   render() {
+//     return (
+//       <div className="Home">
+//         <Head>
+//           <title>Portfolio Project</title>
+//           <meta charSet="utf-8" />
+//           <meta name="description" content="Portfolio " />
+//           <meta name="viewport" content="width=device-width, initial-scale=1" />
+//           {/* <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=3.0, minimum-scale=0.86"></meta> */}
+//         </Head>
+//         <div className="BreadCrumbIndex">
+//           <Navigation current={this.props.url.pathname} />
+//         </div>
+//         <div className="Intro">
+//           <div className="Text">
+//             Hello, my name is Abraham Bueno, and I'm a Full Stack Web Developer
+//           </div>
+//           <div className="Text">The Stack I use is Mongo DB, Express, React, Redux, and Node.</div>
+//         </div>
+//         <div className="ParticlesStyleIndex">
+//           <Particles
+//             params={{
+//               particles: {
+//                 number: {
+//                   value: 70,
+//                 },
+//                 line_linked: {
+//                   shadow: {
+//                     enable: true,
+//                     color: '#3CA9D1',
+//                     blur: 0,
+//                   },
+//                 },
+//               },
+//               interactivity: {
+//                 events: {
+//                   onhover: {
+//                     enable: true,
+//                     mode: 'grab',
+//                   },
+//                 },
+//               },
+//             }}
+//             className="ParticlesShome"
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default withLayout(App);
 
-              }
+const Index = () => (
+  <div style={{ padding: '10px 45px' }}>
+    <Head>
+      <title>Index page</title>
+      <meta name="description" content="This is the description of the Index page" />
+    </Head>
+    <p>Content on Index page</p>
+    <Button variant="contained">MUI button</Button>
+  </div>
+);
 
-            }}
-            className="ParticlesShome"
-          />
-        </div>
-
-
-
-      </div>
-    )
-  }
-}
+export default withLayout(Index);
 // export default (props) => (
 //     <div className="Home" >
 //         <Navigation current={props.match.path} />
@@ -101,7 +111,7 @@ export default class App extends Component {
 //             width: '100%',
 //             height: '20%',
 //             background: '#024'
-//             // backgroundImage: `url(${logo})` 
+//             // backgroundImage: `url(${logo})`
 //           }}
 //         />
 //         <div className="One">
