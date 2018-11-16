55a3d7c3c4f8f20a37858840da6b5d9ea53f17f1
Abraham Bueno <abueno@localhost.localdomain>
2018-11-15 22:51:53 -0600

    Still trying different things, starting to get things in control.

--- pages/index.js
+++ pages/index.js
@@ -58,10 +58,28 @@ class App extends Component {
           <Navigation current={this.props.url.pathname} />
         </div> */}
         <div className="Intro">
-          <div className="Text">
+          {/* <div className="Text">
             Hello, my name is Abraham Bueno, and I'm a Full Stack Web Developer
           </div>
-          <div className="Text">The Stack I use is Mongo DB, Express, React, Redux, and Node.</div>
+          <div className="Text">The Stack I use is Mongo DB, Express, React, Redux, and Node.</div> */}
+
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" color="inherit" noWrap>
+                {/* Permanent drawer */}
+                <Navigation current={this.props.url.pathname} />
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Typography variant="h3" color="primary" className="Text" paragraph>
+              Hello, my name is Abraham Bueno, and I'm a Full Stack Web Developer
+            </Typography>
+            <Typography variant="h3" color="primary" className="Text" paragraph>
+              The Stack I use is Mongo DB, Express, React, Redux, and Node.
+            </Typography>
+          </main>
         </div>
         <div className="ParticlesStyleIndex">
           <Particles
@@ -89,41 +107,6 @@ class App extends Component {
             }}
             className="ParticlesShome"
           />
-          <AppBar position="fixed" className={classes.appBar}>
-            <Toolbar>
-              <Typography variant="h6" color="inherit" noWrap>
-                {/* Permanent drawer */}
-                <Navigation current={this.props.url.pathname} />
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <main className={classes.content}>
-            <div className={classes.toolbar} />
-            <Typography paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
-              elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-              hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-              velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-              Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-              viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
-              Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
-              at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
-              ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-            </Typography>
-            <Typography paragraph>
-              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-              facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-              tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-              volutpat consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at
-              quis risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra
-              accumsan in. In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec
-              nam aliquam sem et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-              tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis eleifend.
-              Commodo viverra maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
-              aliquam ultrices sagittis orci a.
-            </Typography>
-          </main>
         </div>
       </div>
     );
