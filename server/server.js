d50c5c0f7f915659c4a3333ac68028d69fa27960
Abraham Bueno <abueno@localhost.localdomain>
2018-11-20 22:01:50 -0600

    Rewriting the server setup with the routes.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,37 @@
-// const express = require('express')
-// const next = require('next')
-import next from 'next';
 import express from 'express';
+import session from 'express-session';
+import mongoSessionStore from 'connect-mongo';
+import next from 'next';
+import mongoose from 'mongoose';
 
+
+import auth from './google';
+import api from './api';
+import routesWithSlug from './routesWithSlug';
+import { setupGithub as github } from './github';
+
+import logger from './logs';
+
+require('dotenv').config();
 const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+mongoose.connect(
+  MONGO_URL,
+  options,
+);
+
+
+const port = process.env.PORT || 8000;
+const ROOT_URL = process.env.ROOT_URL || `http://localhost:${port}`;
+
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const port = process.env.PORT || 8000;
 app
   .prepare()
   .then(() => {
