c630749b61dc98c8cdcba88a44832db389aa35e3
Abraham Bueno -- FULL STACK WEB DEVELOPER <35821558+abrambueno1992@users.noreply.github.com>
2022-07-27 23:09:45 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
-module.exports = {
+const withCSS = require('@zeit/next-css');
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  webpack (config) {
+    config.module.rules.push({
+      test: /\.(png|jp2|webp|svg|eot|otf|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          publicPath: './',
+          outputPath: 'static/',
+          name: '[name].[ext]',
+          fallback: 'responsive-loader',
+  quality: 85
+        }
+      }
+    })
+
+    return config
+  },
   reactStrictMode: true,
-};
+}
+
+module.exports = nextConfig
