b2ca73da5465e07ffbbb49edcccfcf17e918f576
abrambueno1992 <abrambueno1992@gmail.com>
2022-07-27 23:36:37 -0400

    hannah

---
+++ pages/hannah.js
@@ -0,0 +1,23 @@
+import Particles from 'react-tsparticles';
+// import './styles/Home.css';
+import Head from 'next/head';
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+import withLayout from '../lib/withLayout';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+          Hannah was here!
+      </div>
+
+    )
+  }
+}
+export default App
